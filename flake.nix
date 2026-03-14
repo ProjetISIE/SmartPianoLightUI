24ab78f4ec8827674b5eb45d1a4741da32d0be1c
Guilhem Fauré <pro@guilhemfau.re>
2026-03-14 01:10:42 +0100

    build: crossPackages

--- flake.nix
+++ flake.nix
@@ -1,5 +1,9 @@
 {
-  description = "Nix flake C++23 development environment";
+  description = "Nix flake C++23 cross development environment";
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     engine.url = "github:ProjetISIE/SmartPianoEngine";
@@ -11,16 +15,14 @@
       engine,
     }:
     let
-      systems =
+      localSystems = [
+        "x86_64-linux" # "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      crossSystem = "aarch64-linux";
+      forSystems = f: nixpkgs.lib.genAttrs localSystems (system: f (import nixpkgs { inherit system; }));
+      forCross =
         f:
-        let
-          localSystems = [
-            "aarch64-linux"
-            "x86_64-linux"
-            "aarch64-darwin"
-          ];
-          crossSystem = "aarch64-linux";
-        in
         nixpkgs.lib.genAttrs localSystems (
           localSystem:
           f (import nixpkgs { inherit localSystem; }) (
@@ -32,15 +34,18 @@
         );
     in
     {
-      packages = systems (
+      packages = forSystems (pkgs: rec {
+        smart-piano = pkgs.callPackage ./ui.nix {
+          inherit self pkgs;
+          inherit (pkgs) glfw;
+          engine = engine.packages.${pkgs.stdenv.hostPlatform.system}.default;
+          stdenv = pkgs.clangStdenv;
+        };
+        default = smart-piano;
+      });
+      crossPackages = forCross (
         pkgs: crossPkgs: rec {
-          smart-piano = pkgs.callPackage ./ui.nix {
-            inherit self pkgs;
-            inherit (pkgs) glfw;
-            engine = engine.packages.${pkgs.stdenv.hostPlatform.system}.default;
-            stdenv = pkgs.clangStdenv;
-          };
-          cross-smart-piano = crossPkgs.callPackage ./ui.nix {
+          smart-piano = crossPkgs.callPackage ./ui.nix {
             inherit self;
             inherit (crossPkgs) glfw;
             engine = engine.packages.${crossPkgs.stdenv.hostPlatform.system}.default;
@@ -48,44 +53,41 @@
             stdenv = crossPkgs.clangStdenv;
           };
           default = smart-piano;
-          cross = cross-smart-piano;
         }
       );
-      devShells = systems (
-        pkgs: crossPkgs: {
-          default =
-            pkgs.mkShell.override
-              {
-                stdenv = pkgs.clangStdenv; # Clang instead of GCC
-              }
-              rec {
-                packages = with pkgs; [
-                  clang-tools # Clang CLIs, including LSP
-                  clang-uml # UML diagram generator
-                  cmake-format # CMake formatter
-                  cmake-language-server # Cmake LSP
-                  # cppcheck # C++ Static analysis
-                  doxygen # Documentation generator
-                  # fluidsynth # JACK Synthesizer
-                  lldb # Clang debug adapter
-                  # qsynth # FluidSynth GUI
-                  # socat # Serial terminal for manual testing
-                  # valgrind # Debugging and profiling
-                ];
-                nativeBuildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.nativeBuildInputs;
-                buildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.buildInputs;
-                # Export compile commands JSON for LSP and other tools
-                shellHook = ''
-                  export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
-                  # export XDG_SESSION_TYPE=wayland
-                  # export SDL_VIDEODRIVER=wayland 
-                  # export _JAVA_AWT_WM_NONREPARENTING=1
-                  mkdir --verbose build
-                  cd build
-                  cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-                '';
-              };
-        }
-      );
+      devShells = forSystems (pkgs: {
+        default =
+          pkgs.mkShell.override
+            {
+              stdenv = pkgs.clangStdenv; # Clang instead of GCC
+            }
+            rec {
+              packages = with pkgs; [
+                clang-tools # Clang CLIs, including LSP
+                clang-uml # UML diagram generator
+                cmake-format # CMake formatter
+                cmake-language-server # Cmake LSP
+                # cppcheck # C++ Static analysis
+                doxygen # Documentation generator
+                # fluidsynth # JACK Synthesizer
+                lldb # Clang debug adapter
+                # qsynth # FluidSynth GUI
+                # socat # Serial terminal for manual testing
+                # valgrind # Debugging and profiling
+              ];
+              nativeBuildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.nativeBuildInputs;
+              buildInputs = self.packages.${pkgs.stdenv.hostPlatform.system}.smart-piano.buildInputs;
+              # Export compile commands JSON for LSP and other tools
+              shellHook = ''
+                export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+                # export XDG_SESSION_TYPE=wayland
+                # export SDL_VIDEODRIVER=wayland 
+                # export _JAVA_AWT_WM_NONREPARENTING=1
+                mkdir --verbose build
+                cd build
+                cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+              '';
+            };
+      });
     };
 }
